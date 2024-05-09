9b220b3b19185d64275bec941dbae3d7cfcbaf0d
taelyon <taelyon@naver.com>
2024-05-10 00:07:04 +0900

    update

--- Investar/DBUpdater_new.py
+++ Investar/DBUpdater_new.py
@@ -11,6 +11,8 @@ yf.pdr_override()
 requests.packages.urllib3.disable_warnings()
 import warnings
 warnings.filterwarnings('ignore')
+import re
+from sqlalchemy import create_engine
 
 class DBUpdater:
     def __init__(self):
@@ -307,6 +309,83 @@ class DBUpdater:
         print("Waiting for next update ({}) ...".format(tmnext.strftime('%Y-%m-%d %H:%M')))
         t.start()
 
+class MarketDB:
+    def __init__(self):
+        # SQLite3용 SQLAlchemy 엔진 생성
+        self.engine = create_engine('sqlite:///investar.db', echo=False)
+        self.codes = {}
+        self.get_comp_info()
+
+    def __del__(self):
+        pass
+
+    def get_comp_info(self):
+        sql = "SELECT * FROM company_info"
+        stock_list = pd.read_sql(sql, self.engine)
+        for idx in range(len(stock_list)):
+            self.codes[stock_list['code'].values[idx]] = stock_list['company'].values[idx]
+        return stock_list
+
+    def get_daily_price(self, code, start_date=None, end_date=None):
+        if start_date is None:
+            one_year_ago = datetime.today() - timedelta(days=365)
+            start_date = one_year_ago.strftime('%Y-%m-%d')
+            print(f"start_date is initialized to '{start_date}'")
+        else:
+            start_lst = re.split(r'\D+', start_date)
+            if start_lst[0] == '':
+                start_lst = start_lst[1:]
+            start_year = int(start_lst[0])
+            start_month = int(start_lst[1])
+            start_day = int(start_lst[2])
+            if start_year < 1900 or start_year > 2200:
+                print(f"ValueError: start_year({start_year:d}) is wrong.")
+                return
+            if start_month < 1 or start_month > 12:
+                print(f"ValueError: start_month({start_month:d}) is wrong.")
+                return
+            if start_day < 1 or start_day > 31:
+                print(f"ValueError: start_day({start_day:d}) is wrong.")
+                return
+            start_date = f"{start_year:04d}-{start_month:02d}-{start_day:02d}"
+
+        if end_date is None:
+            end_date = datetime.today().strftime('%Y-%m-%d')
+            print(f"end_date is initialized to '{end_date}'")
+        else:
+            end_lst = re.split(r'\D+', end_date)
+            if end_lst[0] == '':
+                end_lst = end_lst[1:]
+            end_year = int(end_lst[0])
+            end_month = int(end_lst[1])
+            end_day = int(end_lst[2])
+            if end_year < 1800 or end_year > 2200:
+                print(f"ValueError: end_year({end_year:d}) is wrong.")
+                return
+            if end_month < 1 or end_month > 12:
+                print(f"ValueError: end_month({end_month:d}) is wrong.")
+                return
+            if end_day < 1 or end_day > 31:
+                print(f"ValueError: end_day({end_day:d}) is wrong.")
+                return
+            end_date = f"{end_year:04d}-{end_month:02d}-{end_day:02d}"
+
+        codes_keys = list(self.codes.keys())
+        codes_values = list(self.codes.values())
+        if code in codes_keys:
+            pass
+        elif code in codes_values:
+            idx = codes_values.index(code)
+            code = codes_keys[idx]
+        else:
+            print(f"ValueError: Code({code}) doesn't exist.")
+            return
+
+        sql = f"SELECT * FROM daily_price WHERE code = '{code}' AND date >= '{start_date}' AND date <= '{end_date}'"
+        df = pd.read_sql(sql, self.engine)
+        df.index = df['date']
+        return df
+    
 if __name__ == '__main__':
     dbu = DBUpdater()
     dbu.execute_daily()
